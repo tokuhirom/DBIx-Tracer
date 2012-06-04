8d9cb6d71518576c909bce6757822659ec0f05e6
tokuhirom <tokuhirom@gmail.com>
2012-06-05 07:26:19 +0900

    more test cases

--- t/setup_mysqld.pl
+++ t/setup_mysqld.pl
@@ -4,5 +4,5 @@ my $mysqld = t::Util->setup_mysqld or die;
 $ENV{__TEST_DBIxTracer} = encode_json { %$mysqld } if $mysqld;
 $|++;
 print "export __TEST_DBIxTracer='$ENV{__TEST_DBIxTracer}'\n";
-sleep 100;
+sleep 500;
 print "fin.\n";
